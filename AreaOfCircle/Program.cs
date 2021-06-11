b01cfa85f5af6192bb960d87c7661c0be4ce79ae
Mark Anderson <markanderson8162@gmail.com>
2021-06-11 17:32:48 -0500

    created files to solve for the area of a circle

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What is the radius of your circle?");
+			var radius = double.Parse(Console.ReadLine());
+			double circleArea = Math.PI * (radius * radius);
+			Console.WriteLine($"The area of your circle is {circleArea}");
+		}
+	}
+}
